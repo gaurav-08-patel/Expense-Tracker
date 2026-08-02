0bf6b0e8395544a6a1556f603d6da74fefa7292f
gaurav-08-patel <gauravpatelrounak@gmail.com>
2026-08-02 14:34:04 +0530

    changed dependency of useEffect to re-fetch data when changed

--- components/TrackerCard.jsx
+++ components/TrackerCard.jsx
@@ -64,7 +64,7 @@ export default function TrackerCard({ data, onMenuPress = () => {} }) {
         return () => {
             active = false;
         };
-    }, [data.id]);
+    }, [data]);
 
     const progress = useMemo(() => {
         if (!data.initialAmount) {
